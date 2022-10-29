e0ee736d1dc9b0a2d6e7a920e126f1f4537e039b
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-10-29 23:30:54 +0300

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -21,7 +21,7 @@ class FileStorage:
     # Path to the JSON file.
     __file_path = "file.json"
 
-    def all(self, cls = None):
+    def all(self, cls=None):
         """Returns a dictionary of __objects"""
         return self.__objects
 
@@ -30,16 +30,16 @@ class FileStorage:
         if obj is not None:
             # Concatenate class object name with id.
             key = obj.__class__.__name__+"."+obj.id
-            # update the __object dictionary with the new key and object as valu.
+            # update the __object dictionary with values.
             self.__objects[key] = obj
 
     def save(self):
         """" Serializes __objects to the JSON File(path:__file_path)"""
         j_objects = {}
-        # For the python data to be serialized the data stored inside the __objec
+        # For serialization of the data stored inside the __objec
         # variable as objects must be converted to a dictionary so that python
         # for json to understand using to_dict method of the basemodel class
-        # The result of this will be a key with a unique dictionary as the value.
+        # The result is a unique dictionary as the value.
         for key in self.__objects:
             j_objects[key] = self.__objects[key].to_dict()
         # Convert the dictionary into json and save in __filepath.
@@ -54,22 +54,22 @@ class FileStorage:
         """
 
         try:
-            with open(self.__file_path, 'r', encoding = "utf8") as f:
+            with open(self.__file_path, 'r', encoding="utf8") as f:
                 # The load method deserializes a json string into a python dict
                 obj_dict = json.load(f)
             for obj_item in obj_dict.values():
                 # Extract the class from which to instantiate an object.
                 # Remember we loaded the values not the keys.
                 class_name = obj_item["__class__"]
-                # Since we are creating object instances the attribute __class__\
+                # creation of object instances the attribute __class__\
                 # should not be supplied as part of the arguments to the init
                 # function of the class(identified by class_name) all the other
                 # arguments are valid.
                 del obj_item["__class__"]
-                # Use the previously defined new function to create a new __obj.
+                # Use the defined new function to create a new __obj.
                 # The double asterics expands the dictionary to allow every key
-                # value pair from object_item dict to be passed to the __init__()
+                # value pair from object_item to be passed to the __init__()
                 # method of the class identified by class_name
                 self.new(eval(class_name)(**obj_item))
-        except:
+        except FileNotFoundError:
             pass
