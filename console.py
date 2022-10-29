44d64e4e3f7a1af8dda37927e4f65c8ada7c8e13
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-10-29 22:37:04 +0300

    Update console.py

--- console.py
+++ console.py
@@ -16,8 +16,8 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
-
 class HBNBCommand(cmd.Cmd):
+    
     """
     Entry point of the command interpreter
     """
