15d43c32b770bb47467f610fa24e33140af97779
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-10-29 21:48:50 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""
-Module State class
-"""
+"""Defines the State class."""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
+    """Represent a state.
+    Attributes:
+        name (str): The name of the state.
     """
-    Inherits from BaseModel
-    Public class attribute:
-        name: (str)
-    """
+
     name = ""
