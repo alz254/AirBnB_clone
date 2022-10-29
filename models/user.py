e2e5817e114964679f9c74287aafff231aa848a7
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-10-29 19:20:27 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""Defines the User class."""
+"""
+Class User that inherits from BaseModel
+"""
+
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Represent a User.
-    Attributes:
-        email (str): The email of the user.
-        password (str): The password of the user.
-        first_name (str): The first name of the user.
-        last_name (str): The last name of the user.
-    """
-
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    '''base model class'''
+    email=""
+    password=""
+    first_name=""
+    lastname=""
