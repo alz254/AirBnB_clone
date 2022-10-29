4de1cfb243fa24cb49131451e389cdbfb6f0a344
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-10-29 22:49:19 +0300

    Update console.py

--- console.py
+++ console.py
@@ -16,8 +16,8 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class HBNBCommand(cmd.Cmd):
-    
     """
     Entry point of the command interpreter
     """
@@ -186,5 +186,7 @@ class HBNBCommand(cmd.Cmd):
         elif error_num == 6:
             print("** value missing **")
 if __name__ == "__main__":
+    
+    
     signal(SIGINT, HBNBCommand.handler)
     HBNBCommand().cmdloop()
