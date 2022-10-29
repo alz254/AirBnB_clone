d0fa05fcf6dce68750f4e17ea5ab54062617d06c
Allan Ndonga <93614371+alz254@users.noreply.github.com>
2022-10-29 21:47:12 +0300

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""
-Module Review class
-"""
+"""Defines the Review class."""
 from models.base_model import BaseModel
 
 
 class Review(BaseModel):
+    """Represent a review.
+    Attributes:
+        place_id (str): The Place id.
+        user_id (str): The User id.
+        text (str): The text of the review.
     """
-    Inherits from BaseModel
-    Public class attributes:
-        place_id:            (str) will be Place.id
-        user_id:             (str) will be User.id
-        text:                (str)
-    """
+
     place_id = ""
     user_id = ""
     text = ""
